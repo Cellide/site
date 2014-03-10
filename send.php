76cea2fa4de22f7c494138dfcb15b4c6594b8062
Fernando Piancastelli <fernando@cellide.com>
2014-03-10 13:58:35 -0300

    Customização de formulário de envio

--- send.php
+++ send.php
@@ -1,13 +1,13 @@
 <?php 
 $errors = '';
-$myemail = 'info@stefanoschintu.com';//<-----Put Your email address here.
+$myemail = 'contato@cellide.com';//<-----Put Your email address here.
 if(empty($_POST['name'])  || 
    empty($_POST['company']) || 
    empty($_POST['email']) ||
    empty($_POST['option']) ||
    empty($_POST['project']))
 {
-    $errors .= "\n Error: all fields are required";
+    $errors .= "\n Preencha todos os campos";
 }
 
 $name = $_POST['name']; 
@@ -20,16 +20,16 @@ if (!preg_match(
 "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
 $email_address))
 {
-    $errors .= "\n Error: Invalid email address";
+    $errors .= "\n Endereço de email inválido";
 }
 
 if( empty($errors))
 {
 $to = $myemail;
-$email_subject = "Contact form submission: $name";
-$email_body = "You have received a new message. ".
-" Here are the details:\n Name: $name \n ".
-"Email: $email_address\n Company: $company\n Budget: $option\n Message: $project";
+$email_subject = "Formulario de contato: $name";
+$email_body = "Você recebeu uma nova mensagem. ".
+" Detalhes:\n Nome: $name \n ".
+"Email: $email_address\n Empresa: $company\n Orçamento: $option\n Mensagem: $project";
 $headers = "From: $myemail\n";
 $headers .= "Reply-To: $email_address";
 
