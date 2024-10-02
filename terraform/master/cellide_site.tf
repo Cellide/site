76b8bd99ffc192e376872cd9c420579aa43e31fe
Fernando Piancastelli <fernando.piancastelli@direto.com.br>
2024-10-01 21:16:40 -0300

    first plan

--- terraform/master/cellide_site.tf
+++ terraform/master/cellide_site.tf
@@ -1,9 +1,12 @@
 module "cellide_site" {
-  source         = "cloudposse/s3-website/aws"
-  version        = "0.18.0"
-  namespace      = var.default_tags.owner
-  stage          = var.default_tags.environment
-  name           = "site"
-  hostname       = "www.cellide.com"
-  parent_zone_id = "Z0771358Y03ADAX5GY7J"
+  source             = "cloudposse/s3-website/aws"
+  version            = "0.18.0"
+  namespace          = var.default_tags.owner
+  stage              = var.default_tags.environment
+  name               = "cellide_site"
+  hostname           = "www.cellide.com"
+  parent_zone_id     = "Z0771358Y03ADAX5GY7J"
+  logs_enabled       = false
+  tags               = var.default_tags
+  versioning_enabled = false
 }
