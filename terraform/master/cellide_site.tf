9939356f802e7649587b396fece19ee1807d296b
Fernando Piancastelli <fernando.piancastelli@direto.com.br>
2024-10-01 21:29:50 -0300

    http only unfortunately

--- terraform/master/cellide_site.tf
+++ terraform/master/cellide_site.tf
@@ -1,12 +1,13 @@
 module "cellide_site" {
-  source             = "cloudposse/s3-website/aws"
-  version            = "0.18.0"
-  namespace          = var.default_tags.owner
-  stage              = var.default_tags.environment
-  name               = "cellide_site"
-  hostname           = "www.cellide.com"
-  parent_zone_id     = "Z0771358Y03ADAX5GY7J"
-  logs_enabled       = false
-  tags               = var.default_tags
-  versioning_enabled = false
+  source                  = "cloudposse/s3-website/aws"
+  version                 = "0.18.0"
+  namespace               = var.default_tags.owner
+  stage                   = var.default_tags.environment
+  name                    = "cellide_site"
+  hostname                = "www.cellide.com"
+  parent_zone_id          = "Z0771358Y03ADAX5GY7J"
+  logs_enabled            = false
+  tags                    = var.default_tags
+  versioning_enabled      = false
+  allow_ssl_requests_only = false
 }
